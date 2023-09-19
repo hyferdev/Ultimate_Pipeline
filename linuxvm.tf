4176fa532d959ce6d75d898a5981086c6f8c774d
hyferdev <banyerdes@gmail.com>
2023-09-19 03:24:37 +0000

    Updated VM script to install Docker and Maven

--- linuxvm.tf
+++ linuxvm.tf
@@ -8,8 +8,27 @@ resource "aws_instance" "cicd_instance" {
   vpc_security_group_ids = [aws_security_group.cicd_sg.id]
   user_data = <<-EOF
     #!/bin/bash
-    sudo yum update
-    sudo yum install wget
+    sudo apt update
+    sudo apt install openjdk-11-jre -y
+    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins -y
+    sudo apt install maven -y
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    sudo apt install docker-ce -y
+    git clone https://github.com/hyferdev/Jenkins-Zero-To-Hero.git
+    chown ubuntu:ubuntu -R Jenkins-Zero-To-Hero/
+    cd /Jenkins-Zero-To-Hero/java-maven-sonar-argocd-helm-k8s/spring-boot-app/ && mvn clean package
+    sudo usermod -aG docker jenkins
+    sudo usermod -aG docker ubuntu
+    su ubuntu -c "docker build -t ultimate-cicd-pipeline:v1 /Jenkins-Zero-To-Hero/java-maven-sonar-argocd-helm-k8s/spring-boot-app/"
+    su ubuntu -c "docker run -d -p 8010:8080 -t ultimate-cicd-pipeline:v1"
     EOF
   user_data_replace_on_change = true
   tags = {
