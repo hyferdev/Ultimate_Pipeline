c32d44862c8364393425678121cde88cc79f6560
Desire Banyeretse <125156467+hyferdev@users.noreply.github.com>
2023-09-24 22:59:31 -0400

    Testing security group configs

--- secgroup.tf
+++ secgroup.tf
@@ -1,8 +1,54 @@
 # Security group for CICD instance
 resource "aws_security_group" "cicd_sg" {
   description = "Allow traffic to ports 8080, 8010, & 9000"
-  name_prefix = "cicd-sg-"
-  vpc_id     = data.aws_vpc.default.id
+  name        = "cicd-sg"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Allow SSH from any IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "CICD will be accessed on 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SonarQube accessed on 9000"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Maven accessed on 8010"
+    from_port   = 8010
+    to_port     = 8010
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allows all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "cicd_sg_test" {
+  description = "Allow traffic to ports 8080, 8010, & 9000"
+  name        = "cicd-sg-test"
+  vpc_id      = aws_vpc.cicd_vpc.id
 
   ingress {
     description = "Allow SSH from any IP"
